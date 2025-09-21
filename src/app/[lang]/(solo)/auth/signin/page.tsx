5303addd132151d33877b08897f1258937677cea
Yohana Edward <stunnaedward@gmail.com>
2025-09-22 00:05:17 +0300

    Debug if Amplifly secret is present

--- src/app/[lang]/(solo)/auth/signin/page.tsx
+++ src/app/[lang]/(solo)/auth/signin/page.tsx
@@ -2,5 +2,6 @@ export const dynamic = "force-dynamic";
 import { Signin } from "@/components/Signin";
 
 export default function SignIn() {
+  console.log("Amplify NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET ? "✅ set" : "❌ missing");
   return <Signin/>;
 }
