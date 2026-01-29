1a78e4274470be6055f072b539a161e8994f318d
EliyaDeus <preciouseliya1@gmail.com>
2026-01-29 19:34:49 +0300

    test folder renaming

--- src/components/AuthInitializer/AuthInitializer.jsx
+++ src/components/AuthInitializer/AuthInitializer.jsx
@@ -1,7 +1,7 @@
 'use client';
+import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
 import { useEffect } from 'react';
 import axios from '../../lib/services/config';
-import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
 
 export const AuthInitializer = ({ children }) => {
   const { authData, setAuthValues } = useJumboAuth();
@@ -9,8 +9,8 @@ export const AuthInitializer = ({ children }) => {
   useEffect(() => {
     if (authData?.token) {
       axios.get('/api/auth/validate', {
-        headers: { Authorization: `Bearer ${authData.token}` }
-      })
+        headers: { Authorization: `Bearer ${authData.token}` },
+      });
     }
   }, [authData, setAuthValues]);
 
