fd77c7c8bcff4c370bd6bf6ca6bb9dd259debb28
EliyaDeus <preciouseliya1@gmail.com>
2026-02-09 17:58:41 +0300

    fixed conflicts

--- src/components/processApproval/Requisitions.tsx
+++ src/components/processApproval/Requisitions.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useJumboAuth } from '@/app/providers/vider';
+import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
 import UnauthorizedAccess from '@/shared/Information/UnauthorizedAccess';
 import { PERMISSIONS } from '@/utilities/constants/permissions';
 import JumboListToolbar from '@jumbo/components/JumboList/components/JumboListToolbar';
