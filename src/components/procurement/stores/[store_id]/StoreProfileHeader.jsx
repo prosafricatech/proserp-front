345afaaae5772b643215480d51039851a6001a7d
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-02-12 18:00:17 +0300

    back to stores improved

--- src/components/procurement/stores/[store_id]/StoreProfileHeader.jsx
+++ src/components/procurement/stores/[store_id]/StoreProfileHeader.jsx
@@ -1,5 +1,5 @@
-import { Typography, Button, Stack } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Typography, IconButton, Stack, Tooltip } from '@mui/material';
+import { ArrowBackOutlined } from '@mui/icons-material';
 import { useRouter } from 'next/navigation';
 import { useStoreProfile } from './StoreProfileProvider'
 
@@ -7,22 +7,21 @@ function StoreProfileHeader() {
   const { mainStore } = useStoreProfile();
   const router = useRouter();
   return (
-    <Stack direction="row" alignItems="center" spacing={2} mb={2}>
-      <Button
-        startIcon={<ArrowBackIcon />}
-        variant="outlined"
-        onClick={() => router.push('/procurement/stores')}
-        sx={{ minWidth: 0 }}
-        size='small'
-      >
-        Stores
-      </Button>
-      <div>
-        <Typography variant='h2'>{mainStore.name}</Typography>
+    <Stack direction="row" alignItems="center" spacing={1}>
+      <Tooltip title="Back to Stores">
+        <IconButton
+          size="small"
+          onClick={() => router.push('/procurement/stores')}
+        >
+          <ArrowBackOutlined />
+        </IconButton>
+      </Tooltip>
+      <Stack>
+        <Typography variant='h4'>{mainStore.name}</Typography>
         <Typography variant='body1'>{mainStore.alias}</Typography>
-      </div>
+      </Stack>
     </Stack>
   );
 }
 
-export default StoreProfileHeader
+export default StoreProfileHeader;
