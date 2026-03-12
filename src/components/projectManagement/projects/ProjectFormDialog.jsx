fc3e06bacfcb566c80769665fd4d56be1e5c9a87
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-03-12 19:53:54 +0300

    project

--- src/components/projectManagement/projects/ProjectFormDialog.jsx
+++ src/components/projectManagement/projects/ProjectFormDialog.jsx
@@ -174,20 +174,7 @@ function ProjectForm({setOpenDialog, project = null, reFetchProjectAfterEdit }) 
               </Div>
             </Grid>
           }
-          <Grid size={{xs: 12, md: 12, lg: 12}}>
-            <Div sx={{ mt: 1, mb: 1 }}>
-              <TextField
-                label="Description"
-                size="small"
-                defaultValue={project?.description}
-                multiline={true}
-                minRows={2}
-                fullWidth
-                {...register('description')}
-              />
-            </Div>
-          </Grid>
-          <Grid size={{xs: 12, md: 4, lg: 4}}>
+          <Grid size={{xs: 12, md: 6, lg: 6}}>
             <Div sx={{ mt: 1, mb: 1 }}>
               <DateTimePicker
                 label='Commencement Date'
@@ -210,7 +197,7 @@ function ProjectForm({setOpenDialog, project = null, reFetchProjectAfterEdit }) 
               />
             </Div>
           </Grid>
-          <Grid size={{xs: 12, md: 4, lg: 4}}>
+          <Grid size={{xs: 12, md: 6, lg: 6}}>
             <Div sx={{ mt: 1, mb: 1 }}>
               <DateTimePicker
                 label='Completion Date'
@@ -233,7 +220,7 @@ function ProjectForm({setOpenDialog, project = null, reFetchProjectAfterEdit }) 
               />
             </Div>
           </Grid>
-          <Grid size={{xs: 12, md: 4, lg: 4}}>
+          <Grid size={{xs: 12 }}>
             <Div sx={{ mt: 1, mb: 1 }}>
               <StoreSelector
                 multiple={true}
@@ -246,6 +233,19 @@ function ProjectForm({setOpenDialog, project = null, reFetchProjectAfterEdit }) 
                     shouldDirty: true,
                   });
                 }}
+              />
+            </Div>
+          </Grid>
+          <Grid size={{xs: 12, md: 12, lg: 12}}>
+            <Div sx={{ mt: 1, mb: 1 }}>
+              <TextField
+                label="Description"
+                size="small"
+                defaultValue={project?.description}
+                multiline={true}
+                minRows={2}
+                fullWidth
+                {...register('description')}
               />
             </Div>
           </Grid>
