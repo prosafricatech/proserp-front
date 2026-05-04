d29695b00b9d246f3f493144327c3cbee3dd8ba2
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-05-05 00:59:06 +0300

    user project

--- src/components/projectManagement/projects/project-services.js
+++ src/components/projectManagement/projects/project-services.js
@@ -37,7 +37,6 @@ projectsServices.projectUsersList = async (params) => {
     return response.data;
 }
 
-
 projectsServices.getSubContractMaterialIssued = async (params) => {
     const response = await axios.get(
         `/api/projectManagement/project/${params.subcontract_id}/getSubContractMaterialIssued`,
