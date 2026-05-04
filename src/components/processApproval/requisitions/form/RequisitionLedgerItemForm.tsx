317eebb2be1a910844b0ab13f3d281922b8eb390
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-05-04 11:58:48 +0300

    Subcontract Certificate type

--- src/components/processApproval/requisitions/form/RequisitionLedgerItemForm.tsx
+++ src/components/processApproval/requisitions/form/RequisitionLedgerItemForm.tsx
@@ -63,6 +63,10 @@ function RequisitionLedgerItemForm({
       value: 'purchase',
       label: 'Purchase',
     },
+    {
+      value: 'subcontract_certificate',
+      label: 'Subcontract Certificate',
+    },
   ];
 
   const getEmptyFormValues = () => ({
