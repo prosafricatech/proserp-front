de008a780a877084f5942abe282b8c730fb751fc
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-05-30 05:28:23 +0300

    from live

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -1298,6 +1298,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/api/exports/excel/purchaseRequisition/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/exports/excel/purchaseRequisition">> = Specific
+  const handler = {} as typeof import("../../../src/app/api/exports/excel/purchaseRequisition/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../src/app/api/exports/excel/salesShifts/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/exports/excel/salesShifts">> = Specific
