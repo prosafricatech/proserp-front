0ede756b3203eda20d48beb059c16dd324857f6f
EliyaDeus <preciouseliya1@gmail.com>
2026-05-30 14:30:51 +0300

    fixed infinite loop

--- src/components/processApproval/requisitions/listItem/tabs/ApprovalsActionTail.tsx
+++ src/components/processApproval/requisitions/listItem/tabs/ApprovalsActionTail.tsx
@@ -1,30 +1,33 @@
-'use client'
-import { FactCheckOutlined } from "@mui/icons-material";
-import { 
-  ButtonGroup, 
-  Dialog, 
-  IconButton, 
-  LinearProgress, 
-  Tooltip, 
-  useMediaQuery 
-} from "@mui/material";
-import React, { useContext, useState } from "react";
-import ApprovalForm from "./form/ApprovalForm";
-import { requisitionContext } from "../../../Requisitions";
-import requisitionsServices from "../../../requisitionsServices";
-import { useJumboTheme } from "@jumbo/components/JumboTheme/hooks";
-import { useJumboAuth } from "@/app/providers/JumboAuthProvider";
-import { useQuery } from "@tanstack/react-query";
-import { Requisition } from "@/components/processApproval/RequisitionType";
+'use client';
+import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
+import { requisitionContext } from '@/components/processApproval/Requisitions';
+import requisitionsServices from '@/components/processApproval/requisitionsServices';
+import { Requisition } from '@/components/processApproval/RequisitionType';
+import { useJumboTheme } from '@jumbo/components/JumboTheme/hooks';
+import { FactCheckOutlined } from '@mui/icons-material';
+import {
+  ButtonGroup,
+  Dialog,
+  IconButton,
+  LinearProgress,
+  Tooltip,
+  useMediaQuery,
+} from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext, useState } from 'react';
+import ApprovalForm from './form/ApprovalForm';
 
 interface ApprovalsActionTailProps {
   requisition: Requisition;
+  isPending?: boolean;
+  isEditAction?: boolean;
+  requisitionDetails?: any;
   isExpanded: boolean;
 }
 
-const ApprovalsActionTail: React.FC<ApprovalsActionTailProps> = ({ 
-  requisition, 
-  isExpanded 
+const ApprovalsActionTail: React.FC<ApprovalsActionTailProps> = ({
+  requisition,
+  isExpanded,
 }) => {
   const { theme } = useJumboTheme();
   const [openDialog, setOpenDialog] = useState(false);
@@ -34,43 +37,47 @@ const ApprovalsActionTail: React.FC<ApprovalsActionTailProps> = ({
 
   const { data: requisitionDetails, isPending } = useQuery({
     queryKey: ['requisitionDetails', { id: requisition.id }],
-    queryFn: async () => await requisitionsServices.getRequisitionDetails(requisition.id),
-    enabled: !isEditAction && isExpanded && openDialog,
-    refetchOnWindowFocus: true
+    queryFn: async () =>
+      await requisitionsServices.getRequisitionDetails(requisition.id),
+    // enabled: !isEditAction && isExpanded && openDialog,
+    enabled: isExpanded,
+    refetchOnWindowFocus: true,
   });
 
   if (isPending) {
     return <LinearProgress />;
   }
 
-  const canApprove = hasOrganizationRole(requisition?.next_approval_level?.role?.name as string) &&
-    !(requisition.status?.toLowerCase() === 'suspended');
+  const canApprove =
+    hasOrganizationRole(
+      requisition?.next_approval_level?.role?.name as string
+    ) && !(requisition.status?.toLowerCase() === 'suspended');
 
   return (
     <>
-      <Dialog 
-        maxWidth="lg" 
-        fullWidth 
-        scroll={belowLargeScreen ? 'body' : 'paper'} 
-        fullScreen={belowLargeScreen} 
+      <Dialog
+        maxWidth='lg'
+        fullWidth
+        scroll={belowLargeScreen ? 'body' : 'paper'}
+        fullScreen={belowLargeScreen}
         open={openDialog}
       >
         {requisitionDetails && (
-          <ApprovalForm 
-            toggleOpen={setOpenDialog} 
-            requisition={requisitionDetails} 
+          <ApprovalForm
+            toggleOpen={setOpenDialog}
+            requisition={requisitionDetails}
           />
         )}
       </Dialog>
 
       {canApprove && (
-        <ButtonGroup 
-          variant="outlined" 
-          size="small" 
-          disableElevation 
+        <ButtonGroup
+          variant='outlined'
+          size='small'
+          disableElevation
           sx={{ '& .MuiButton-root': { px: 1 } }}
         >
-          <Tooltip title="Approve Requisition">
+          <Tooltip title='Approve Requisition'>
             <IconButton onClick={() => setOpenDialog(true)}>
               <FactCheckOutlined />
             </IconButton>
