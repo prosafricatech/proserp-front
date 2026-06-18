a572b89e8d83bead6f777803d74aca8ec174ed82
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-06-18 10:41:37 +0300

    claim fix

--- src/components/projectManagement/projects/profile/claims/form/tab/claimedDeliverables/ClaimedDeliverablesItemForm.tsx
+++ src/components/projectManagement/projects/profile/claims/form/tab/claimedDeliverables/ClaimedDeliverablesItemForm.tsx
@@ -210,7 +210,7 @@ const ClaimedDeliverablesItemForm: React.FC<ClaimedDeliverablesItemFormProps> = 
     setIsRetrievingDetails(true);
     try {
       const details = await projectsServices.showDelUncertifiedQTY(delId, claimDate);
-      setValue('response_uncertified_quantity', deliverableItem?.certified_quantity ?? details?.uncertified_quantity ?? 0);
+      setValue('response_uncertified_quantity', deliverableItem ? deliverableItem.certified_quantity + (details?.uncertified_quantity ?? 0) : details?.uncertified_quantity ?? 0);
     } catch (error) {
       console.error('Failed to retrieve uncertified quantity', error);
     } finally {
