b61f21a499f318797b45b0e0594f4a15a70a5a17
EliyaDeus <preciouseliya1@gmail.com>
2026-06-23 13:51:38 +0300

    modified description view on profoma onscreen

--- src/components/pos/proformaInvoices/form/ProformaItemRow.tsx
+++ src/components/pos/proformaInvoices/form/ProformaItemRow.tsx
@@ -54,7 +54,8 @@ function ProformaItemRow({
   vat_percentage = 0,
 }: ProformaItemRowProps) {
   const product = item?.product;
-  const thumbnail = item?.product?.main_photo?.full_path || item?.product?.thumbnail;
+  const thumbnail =
+    item?.product?.main_photo?.full_path || item?.product?.thumbnail;
   const [showForm, setShowForm] = useState(false);
   const vat_factor = vat_percentage * 0.01;
 
