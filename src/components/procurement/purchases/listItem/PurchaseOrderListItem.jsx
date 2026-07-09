a3ebb18dc9e02f22056d1b4daa52bd31c8a2b757
EliyaDeus <preciouseliya1@gmail.com>
2026-07-09 14:26:38 +0300

    add reference on purchase list

--- src/components/procurement/purchases/listItem/PurchaseOrderListItem.jsx
+++ src/components/procurement/purchases/listItem/PurchaseOrderListItem.jsx
@@ -1,4 +1,8 @@
-import React, { createContext, lazy, useState } from 'react';
+import { readableDate } from '@/app/helpers/input-sanitization-helpers';
+import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
+import { PERMISSIONS } from '@/utilities/constants/permissions';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import {
   Accordion,
   AccordionDetails,
@@ -12,22 +16,20 @@ import {
   Tooltip,
   Typography,
 } from '@mui/material';
-import PurchaseOrderListItemAction from './PurchaseOrderListItemAction';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
-import purchaseServices from '../purchase-services';
-import { useJumboAuth } from '@/app/providers/JumboAuthProvider';
 import { useQuery } from '@tanstack/react-query';
-import { readableDate } from '@/app/helpers/input-sanitization-helpers';
-import { PERMISSIONS } from '@/utilities/constants/permissions';
+import React, { createContext, lazy, useState } from 'react';
+import purchaseServices from '../purchase-services';
+import PurchaseOrderListItemAction from './PurchaseOrderListItemAction';
 
 export const listItemContext = createContext({});
 
 const PurchaseOrderGrns = lazy(() => import('./PurchaseOrderGrns'));
-const PurchaseOrderGrnsItemAction = lazy(() => import('./PurchaseOrderGrnsItemAction'));
+const PurchaseOrderGrnsItemAction = lazy(
+  () => import('./PurchaseOrderGrnsItemAction')
+);
 
 const PurchaseOrderListItem = ({ order }) => {
-  const {checkOrganizationPermission} = useJumboAuth()
+  const { checkOrganizationPermission } = useJumboAuth();
   const [expanded, setExpanded] = useState(false);
   const [selectedOrderGrn, setSelectedOrderGrn] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
@@ -42,20 +44,36 @@ const PurchaseOrderListItem = ({ order }) => {
   });
 
   return (
-    <listItemContext.Provider value={{attachDialog, setAttachDialog, purchaseOrderGrns,expanded,setExpanded,selectedOrderGrn,setSelectedOrderGrn,openDialog,setOpenDialog,openDocumentDialog,setOpenDocumentDialog,openEditReceive,setOpenEditReceive}}>
+    <listItemContext.Provider
+      value={{
+        attachDialog,
+        setAttachDialog,
+        purchaseOrderGrns,
+        expanded,
+        setExpanded,
+        selectedOrderGrn,
+        setSelectedOrderGrn,
+        openDialog,
+        setOpenDialog,
+        openDocumentDialog,
+        setOpenDocumentDialog,
+        openEditReceive,
+        setOpenEditReceive,
+      }}
+    >
       <React.Fragment>
         <Accordion
           expanded={expanded}
           square
-          sx={{ 
-            borderRadius: 2, 
+          sx={{
+            borderRadius: 2,
             borderTop: 2,
             borderColor: 'divider',
             '&:hover': {
               bgcolor: 'action.hover',
             },
           }}
-          onChange={()=> setExpanded((prevExpanded) => !prevExpanded)}
+          onChange={() => setExpanded((prevExpanded) => !prevExpanded)}
         >
           <AccordionSummary
             expandIcon={expanded ? <RemoveIcon /> : <AddIcon />}
@@ -66,11 +84,12 @@ const PurchaseOrderListItem = ({ order }) => {
                 alignItems: 'center',
                 '&.Mui-expanded': {
                   margin: '10px 0',
-                }},
+                },
+              },
               '.MuiAccordionSummary-expandIconWrapper': {
                 borderRadius: 1,
                 border: 1,
-                color: 'text.secondary',  
+                color: 'text.secondary',
                 transform: 'none',
                 mr: 1,
                 '&.Mui-expanded': {
@@ -87,44 +106,55 @@ const PurchaseOrderListItem = ({ order }) => {
             <Grid
               container
               spacing={1}
-              alignItems="center"
+              alignItems='center'
               sx={{ width: '100%', m: 0 }}
             >
-              <Grid size={{xs: 6, md: 2.5}}>
+              <Grid size={{ xs: 6, md: 2.5 }}>
                 <ListItemText
                   primary={
                     <>
                       <Stack direction={'row'}>
-                        <Tooltip title="Order No.">
+                        <Tooltip title='Order No.'>
                           <Typography>{order.orderNo}</Typography>
                         </Tooltip>
-                        {
-                          !!order?.requisitionNo &&
-                          <Tooltip title="Requisition No.">
-                            <Typography variant='caption' color={'gray'}> &nbsp;{` - ${order.requisitionNo}`}</Typography>
+                        {!!order?.requisitionNo && (
+                          <Tooltip title='Requisition No.'>
+                            <Typography variant='caption' color={'gray'}>
+                              {' '}
+                              &nbsp;{` - ${order.requisitionNo}`}
+                            </Typography>
                           </Tooltip>
-                        }
+                        )}
                       </Stack>
-                      <Tooltip title="Date">
-                        <Typography variant="caption">
-                          {readableDate(order.order_date)}
-                        </Typography>
-                      </Tooltip>
+                      <Stack direction={'column'}>
+                        <Tooltip title='Date'>
+                          <Typography variant='caption'>
+                            {readableDate(order.order_date)}
+                          </Typography>
+                        </Tooltip>
+                        {order?.reference && (
+                          <Tooltip title={'Reference'}>
+                            <Typography variant='caption' mt={1}>
+                              {order?.reference}
+                            </Typography>
+                          </Tooltip>
+                        )}
+                      </Stack>
                     </>
                   }
                 />
               </Grid>
-              <Grid size={{xs: 6, md: 5}}>
+              <Grid size={{ xs: 6, md: 5 }}>
                 <Tooltip title={'Supplier'}>
                   <Typography>{order.stakeholder.name}</Typography>
                 </Tooltip>
                 {order.cost_centers.length > 0 && (
-                  <Tooltip title="Cost Centers">
+                  <Tooltip title='Cost Centers'>
                     <div>
                       {order.cost_centers.map((cc) => (
                         <Chip
                           key={cc.id}
-                          size="small"
+                          size='small'
                           label={cc.name}
                           style={{ margin: '1px' }}
                         />
@@ -133,10 +163,15 @@ const PurchaseOrderListItem = ({ order }) => {
                   </Tooltip>
                 )}
               </Grid>
-              <Grid size={{xs: 12, md: 4.5}} display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+              <Grid
+                size={{ xs: 12, md: 4.5 }}
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'space-between'}
+              >
                 <Tooltip title={'Status'}>
                   <Chip
-                    size="small"
+                    size='small'
                     label={order.status}
                     color={
                       order.status === 'Completed' ||
@@ -144,51 +179,50 @@ const PurchaseOrderListItem = ({ order }) => {
                       order.status === 'Instantly Received'
                         ? 'success'
                         : order.status === 'Partially Received'
-                        ? 'warning'
-                        : order.status === 'Closed' 
-                        ? 'info'
-                        : 'primary'
+                          ? 'warning'
+                          : order.status === 'Closed'
+                            ? 'info'
+                            : 'primary'
                     }
                   />
                 </Tooltip>
-                {
-                checkOrganizationPermission(PERMISSIONS.PURCHASES_CREATE) &&
+                {checkOrganizationPermission(PERMISSIONS.PURCHASES_CREATE) && (
                   <Tooltip title={'Amount'}>
                     <Typography>
-                      {
-                        (order.amount + order.vat_amount).toLocaleString('en-US', {
-                        style: 'currency',
-                        currency: order.currency.code,
-                      })}
+                      {(order.amount + order.vat_amount).toLocaleString(
+                        'en-US',
+                        {
+                          style: 'currency',
+                          currency: order.currency.code,
+                        }
+                      )}
                     </Typography>
                   </Tooltip>
-                }
+                )}
               </Grid>
             </Grid>
-            <Divider/>
+            <Divider />
           </AccordionSummary>
           <AccordionDetails
-            sx={{ 
-              backgroundColor:'background.paper',
-              marginBottom: 3
+            sx={{
+              backgroundColor: 'background.paper',
+              marginBottom: 3,
             }}
           >
-            {
-            isLoading && <LinearProgress/>
-            }
+            {isLoading && <LinearProgress />}
             <Grid container>
               <Grid size={12} textAlign={'end'}>
                 <PurchaseOrderListItemAction order={order} />
               </Grid>
-              
+
               {/*OrderGrns*/}
-              <PurchaseOrderGrns order={order}/>
+              <PurchaseOrderGrns order={order} />
             </Grid>
           </AccordionDetails>
         </Accordion>
 
-          {/*OrderGrnsItemAction*/}
-          <PurchaseOrderGrnsItemAction order={order}/>
+        {/*OrderGrnsItemAction*/}
+        <PurchaseOrderGrnsItemAction order={order} />
       </React.Fragment>
     </listItemContext.Provider>
   );
