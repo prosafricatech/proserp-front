11927e4a66be477d0106d35380d90ae097273921
EliyaDeus <preciouseliya1@gmail.com>
2026-07-22 14:22:59 +0300

    approved payroll runs

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -3107,6 +3107,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/api/humanResources/payrollRuns/[id]/salary-sheet/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/humanResources/payrollRuns/[id]/salary-sheet">> = Specific
+  const handler = {} as typeof import("../../../src/app/api/humanResources/payrollRuns/[id]/salary-sheet/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../src/app/api/humanResources/payrollRuns/[id]/simulate/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/humanResources/payrollRuns/[id]/simulate">> = Specific
